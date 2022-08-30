cfa1178df4dc901f469c53516526153d4dd5b93b
Kevin Junger <kevin.junger@outlook.fr>
2022-08-30 17:17:20 +0200

    express

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const http = require("http")
+const app = require("./app")
 
-const server = http.createServer((req, res) => { res.end("Salut !") })
+app.set('port', process.env.PORT || 3000)
+const server = http.createServer(app)
 
 server.listen(process.env.PORT || 3000)
